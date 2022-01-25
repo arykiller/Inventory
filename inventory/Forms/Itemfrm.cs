c41fabf8c788517c60f7a71b65c94ef756ee169e
Ammar Talal <36006767+arykiller@users.noreply.github.com>
2022-01-25 21:55:02 +0300

    Create Itemfrm.cs

---
+++ inventory/Forms/Itemfrm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inventory
+{
+    public partial class Itemfrm : Form
+    {
+        public Itemfrm()
+        {
+            InitializeComponent();
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void bindingNavigator1_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Program.Back();
+            this.Hide();
+        }
+    }
+}
