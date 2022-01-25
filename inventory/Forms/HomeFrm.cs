7022a218365692096823dbd358eac4c9ad1b9b9c
Ammar Talal <36006767+arykiller@users.noreply.github.com>
2022-01-25 21:54:52 +0300

    Create HomeFrm.cs

---
+++ inventory/Forms/HomeFrm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inventory
+{
+    public partial class HomeFrm : Form
+    {
+        public HomeFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Itemfrm itemfrm = new Itemfrm();
+            itemfrm.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            UsersFrm usersfrm = new UsersFrm();
+            usersfrm.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            InventoryFrm inventoryfrm = new InventoryFrm(); 
+            inventoryfrm.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TransFrm transfrm = new TransFrm();
+            transfrm.Show();
+            this.Hide();
+        }
+
+        private void HomeFrm_Load(object sender, EventArgs e)
+        {
+            if (Program.role != 0)
+            {
+                button3.Enabled = false;
+            }
+        }
+
+        private void LogoutBt_Click(object sender, EventArgs e)
+        {
+            LoginFrm login = new LoginFrm();
+            login.Show();
+            this.Hide();
+        }
+    }
+}
