4b4d7e1f8d06e56635bd292b25adc4d7faba8f3b
Ammar Talal <36006767+arykiller@users.noreply.github.com>
2022-01-25 21:55:14 +0300

    Create TransFrm.cs

---
+++ inventory/Forms/TransFrm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inventory
+{
+    public partial class TransFrm : Form
+    {
+        public TransFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void BackBt_Click(object sender, EventArgs e)
+        {
+            Program.Back();
+            this.Hide();
+        }
+    }
+}
