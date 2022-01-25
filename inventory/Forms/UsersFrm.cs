65243e165a27871023904f1b8e1874aeea8c82a3
Ammar Talal <36006767+arykiller@users.noreply.github.com>
2022-01-25 21:55:19 +0300

    Create UsersFrm.cs

---
+++ inventory/Forms/UsersFrm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace inventory
+{
+    public partial class UsersFrm : Form
+    {
+        public UsersFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsersFrm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BackBt_Click(object sender, EventArgs e)
+        {
+            Program.Back();
+            this.Hide();
+        }
+    }
+}
