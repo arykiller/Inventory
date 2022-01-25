bfb30c2a8fb6cdcba9748cd2d2fa6ca14f2a4139
Ammar Talal <36006767+arykiller@users.noreply.github.com>
2022-01-25 21:55:56 +0300

    Create Program.cs

---
+++ inventory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inventory
+{
+    internal static class Program
+    {
+        //Define Global Login Variables
+        public static int role;
+        public static string userName;
+        public static string password;
+        //Function To go back in the Forms
+        public static void Back()
+        {
+            HomeFrm homeFrm = new HomeFrm();
+            homeFrm.Show();
+        }
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginFrm());
+        }
+    }
+}
