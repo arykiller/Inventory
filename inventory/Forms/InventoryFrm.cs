8e46cfc606816c66e911b18fbf8075aab5bd78a6
Ammar Talal <36006767+arykiller@users.noreply.github.com>
2022-01-25 21:54:56 +0300

    Create InventoryFrm.cs

---
+++ inventory/Forms/InventoryFrm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace inventory
+{
+    public partial class InventoryFrm : Form
+    {
+        public InventoryFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InventoryFrm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Program.Back();
+            this.Hide();
+        }
+    }
+}
